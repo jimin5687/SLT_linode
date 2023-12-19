bea34eadbc6cf5435125164dd36fc5f47780f1c5
jimin5687 <145430529+jimin5687@users.noreply.github.com>
2023-12-19 19:32:25 +0900

    twelve local changes

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('other/', include('other.url')),
-    path('mysite/', include('mysite.url'))
+    path('mysite/', include('mysite.url')),
 ]
